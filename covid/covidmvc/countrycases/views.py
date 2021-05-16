333bb2d24620a16555278ece8fee0db283d4a182
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-05-16 10:58:07 -0300

    add template

--- covid/covidmvc/countrycases/views.py
+++ covid/covidmvc/countrycases/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Cases
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    cases_list = Cases.objects.all()
+    template = loader.get_template('countrycases/index.html')
+    context = {
+        'cases_list': cases_list
+    }
+    return HttpResponse(template.render(context, request))
--- covid/covidmvc/covidmvc/urls.py
+++ covid/covidmvc/covidmvc/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('countrycases/', include('countrycases.urls')),
+    path('', include('countrycases.urls')),
     path('admin/', admin.site.urls),
 ]
