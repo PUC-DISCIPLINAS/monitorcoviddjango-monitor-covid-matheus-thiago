b25a660307e80e5e89c14349b013288e4f90ad7c
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-05-16 13:54:24 -0300

    feat: order by confirmed cases

--- covid/covidmvc/countrycases/views.py
+++ covid/covidmvc/countrycases/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Cases
 
 def index(request):
-    cases_list = Cases.objects.all()
+    cases_list = Cases.objects.order_by('-confirmed_cases')
     context = {
         'cases_list': cases_list
     }
