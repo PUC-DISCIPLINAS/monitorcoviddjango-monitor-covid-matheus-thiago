7440e76c179fc6f8963b829bf487ad20d8942261
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-05-16 11:58:42 -0300

    add table in template

--- covid/covidmvc/countrycases/views.py
+++ covid/covidmvc/countrycases/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Cases
 
 def index(request):
     cases_list = Cases.objects.all()
-    template = loader.get_template('countrycases/index.html')
     context = {
         'cases_list': cases_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'countrycases/index.html', context)
